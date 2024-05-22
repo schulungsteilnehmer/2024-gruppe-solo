cbe60433c1403aea07f08b07b82874d15c9be138
schulungsteilnehmer <schulungsteilnehmer@outlook.com>
2024-05-22 14:49:33 +0200

    Verbesserung des Programms

--- Program.cs
+++ Program.cs
@@ -16,9 +16,10 @@ namespace msg_David_Sprint__Tag_1_
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Guten Tag!");
-			
+			int i = 0;
+			int AnzahlUnterkurse = 0;
 			// TODO: Implement Functionality Here
-			Double Fach1, Fach2, Fach3, Fach4, Fach5, Fach6, Fach7, Fach8;
+			int Fach1, Fach2, Fach3, Fach4, Fach5, Fach6, Fach7, Fach8;
 			Double Notendurchschnitt;
 			String Name;
 			String Datum;
@@ -33,6 +34,11 @@ namespace msg_David_Sprint__Tag_1_
 			}
 			Console.Write("Datum: ");
 			Datum = Console.ReadLine();
+			if (!Regex.IsMatch(Datum, @"^[1-2024,.,,]")) 
+			{
+				Console.Write("Ungültiges Datum!");
+				Console.ReadKey(true);
+			}
 			Console.WriteLine();
 			Console.Write("Fehltage: ");
 			Fehltage = Convert.ToInt32(Console.ReadLine());
@@ -40,23 +46,23 @@ namespace msg_David_Sprint__Tag_1_
 			unentschuldigteFehltage = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine();
 			Console.Write("Wie viele Punkte in Leistungkurs Informatik? ");
-			Fach1 = Convert.ToDouble(Console.ReadLine());
+			Fach1 = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Wie viele Punkte in Leistungskurs Mathematik? ");
-			Fach2 = Convert.ToDouble(Console.ReadLine());
+			Fach2 = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Wie viele Punkte in Englisch? ");
-			Fach3 = Convert.ToDouble(Console.ReadLine());
+			Fach3 = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Wie viele Punkte in Politik? ");
-			Fach4 = Convert.ToDouble(Console.ReadLine());
+			Fach4 = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Wie viele Punkte in Deutsch? ");
-			Fach5 = Convert.ToDouble(Console.ReadLine());
+			Fach5 = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Wie viele Punkte in Geschichte? ");
-			Fach6 = Convert.ToDouble(Console.ReadLine());
+			Fach6 = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Wie viele Punkte in Kunst? ");
-			Fach7 = Convert.ToDouble(Console.ReadLine());
+			Fach7 = Convert.ToInt32(Console.ReadLine());
 			Console.Write("Wie viele Punkte in Sport? ");
-			Fach8 = Convert.ToDouble(Console.ReadLine());
-			Notendurchschnitt = Math.Round((Fach1 *2 + Fach2 *2 + Fach3+Fach4+Fach5+Fach6+Fach7+Fach8)/10,1);
-			
+			Fach8 = Convert.ToInt32(Console.ReadLine());
+			Notendurchschnitt = Convert.ToDouble((Fach1 *2 + Fach2 *2 + Fach3+Fach4+Fach5+Fach6+Fach7+Fach8)/10);
+			Math.Round(Notendurchschnitt,1);
 			
 			if (Notendurchschnitt > 15)
 			{
@@ -158,6 +164,16 @@ namespace msg_David_Sprint__Tag_1_
 			Console.WriteLine(Fach8);
 			Console.WriteLine();
 			
+			int [] UnterkursArray = new int[] {Fach1,Fach2,Fach3,Fach4,Fach5,Fach6,Fach7,Fach8};
+			int Vergleichszahl = 5;
+			for (i = 0; i < UnterkursArray.Length; i++)
+			{
+				if (UnterkursArray [i] < Vergleichszahl)
+				{
+					AnzahlUnterkurse = AnzahlUnterkurse + 1;
+				}
+			}
+			
 			if (Notendurchschnitt > 5)
 			{
 				if (unentschuldigteFehltage > 30)
@@ -167,16 +183,29 @@ namespace msg_David_Sprint__Tag_1_
 					Console.ForegroundColor = ConsoleColor.Gray;
 					Console.Write("Der Notendurschnitt beträgt: ");
 					Console.Write(Notendurchschnitt.ToString());
+					Console.WriteLine();
+					Console.WriteLine("Der Schüler verzeichnet zu viele unentschuldigte Fehltage.");
+					Console.ReadKey(true);
+				}
+				if (AnzahlUnterkurse > 2)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("Der Schüler wird nicht versetzt.");
+					Console.ForegroundColor = ConsoleColor.Gray;
+					Console.Write("Der Notendurschnitt beträgt: ");
+					Console.Write(Notendurchschnitt.ToString());
+					Console.ReadLine();
+					Console.WriteLine("Der Schüler verzeichnet zu viele Unterkurse.");
 					Console.ReadKey(true);
 				}
 				else 
 				{
-				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine("Der Schüler wird versetzt.");
-				Console.ForegroundColor = ConsoleColor.Gray;
-				Console.Write("Der Notendurschnitt beträgt: ");
-				Console.Write(Notendurchschnitt.ToString());
-				Console.ReadKey(true);
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine("Der Schüler wird versetzt.");
+					Console.ForegroundColor = ConsoleColor.Gray;
+					Console.Write("Der Notendurschnitt beträgt: ");
+					Console.Write(Notendurchschnitt.ToString());
+					Console.ReadKey(true);
 				}
 			}	
 			if (Notendurchschnitt < 5)
@@ -186,8 +215,9 @@ namespace msg_David_Sprint__Tag_1_
 				Console.ForegroundColor = ConsoleColor.Gray;
 				Console.Write("Der Notendurschnitt beträgt: ");
 				Console.Write(Notendurchschnitt.ToString());
+				Console.WriteLine();
+				Console.WriteLine("Der Schüler verzeichnet einen zu niedrigen Notendurchschnitt");
 			}
-			Console.ReadLine();
 			}
 		}
 	}
